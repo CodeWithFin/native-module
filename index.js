807a02b2fa33ea8fd243f610c28c853fc040000a
Finley <Finley.mwachia12@gmail.com>
2024-10-08 19:01:04 +0300

    add code for reading through the message.txt

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const fs = require("fs");
-fs.writeFile("message.txt","Hello from Finley",(err)=>{
+fs.writeFile("message.txt","Hello from Finley.I am really looking forward to working with you guys",(err)=>{
     if (err) throw err;
     console.log("The file has been saved");
 });
+
+const {open} = require('node:fs/promises');
+(async()=>{
+    const file = await open("message.txt");
+
+    for await (const line of file.readLines()){
+        console.log(line);
+    }
+})();
